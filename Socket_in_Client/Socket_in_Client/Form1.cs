d28c96636f67c664392daf226a68297ac36633ae
Your Name <you@example.com>
2020-09-20 15:01:10 +0430

    it works with pop ip

---
+++ Socket_in_Client/Socket_in_Client/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Socket_in_Client
+{
+
+    
+    public partial class Form1 : Form
+    {
+        public static string ip;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ip = textBox1.Text;
+
+
+            Client cl = new Client();
+            cl.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
